70d4bd45d1a8a44a48cc2af0be12d8eb03aaef19
JNBGames <56099643+JNBGames@users.noreply.github.com>
2019-10-03 00:42:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ var options =
 	},
 	identity: 
 	{
-		username: "lucasvictor97",
-		password: "oauth:3n9yvbgphk2rrmfka7ese9q65yzi5f",  
+		username: "jnbgames24mg",
+		password: "oauth:zfm5tp0uas3k1j3y9m7a1btqct0mdl",  
 	},
 	channels: ['#gaules', '#inr1tv', '#marcobc', '#murilo_rt', '#jeffaocs', '#pescocofino', '#csrfps', '#mibrtv', '#marcaorx', '#sheviiioficial', '#brnwowzk1', '#gabepeixe', '#stev3z', '#Vovo', '#furiatv', '#narigafps', '#stereonline', '#skipnho', '#skipnholive', '#saullo', '#mch_agg',] //ADICIONA OS CANAIS AQ COM '#NOME'
 };
