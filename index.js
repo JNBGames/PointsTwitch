b57c196ba53dad02c615987c3c8aa8cdccb135b9
JNBGames <56099643+JNBGames@users.noreply.github.com>
2019-10-19 00:35:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var options =
 		username: "jnbgames24mg",
 		password: "oauth:zfm5tp0uas3k1j3y9m7a1btqct0mdl",  
 	},
-	channels: ['#gaules', '#inr1tv', '#marcobc', '#murilo_rt', '#jeffaocs', '#pescocofino', '#csrfps', '#mibrtv', '#marcaorx', '#sheviiioficial', '#brnwowzk1', '#gabepeixe', '#stev3z', '#Vovo', '#furiatv', '#narigafps', '#stereonline', '#skipnho', '#skipnholive', '#saullo', '#mch_agg', '#johnpittertv' ] //ADICIONA OS CANAIS AQ COM '#NOME'
+	channels: ['#gaules', '#inr1tv', '#marcobc', '#murilo_rt', '#jeffaocs', '#pescocofino', '#csrfps', '#mibrtv', '#marcaorx', '#sheviiioficial', '#brnwowzk1', '#gabepeixe', '#stev3z', '#Vovo', '#furiatv', '#narigafps', '#stereonline', '#skipnho', '#skipnholive', '#saullo', '#mch_agg', ' #johnpittertv ',] //ADICIONA OS CANAIS AQ COM '#NOME'
 };
 
 var client = new tmi.client(options);
